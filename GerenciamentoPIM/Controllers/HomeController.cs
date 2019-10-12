a202e05f336fd0efdbbee3a85414c4c5058eabf9
Guilherme Siqueira Leite <ki_lua_741@hotmail.com>
2019-10-12 16:35:09 -0300

    Adicionar arquivos de projeto.

---
+++ GerenciamentoPIM/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using GerenciamentoPIM.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GerenciamentoPIM.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+       
+        public ActionResult Cadastrar(Aluno aluno)
+        {
+
+            if (ModelState.IsValid)
+            {
+                aluno.Cadastrar(aluno);
+
+                return Index();
+            }
+
+            return View();
+        }
+    }
+}
---
+++ GerenciamentoPIM/Models/Aluno.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.OleDb;
+using System.Configuration;
+using System.Data;
+
+namespace GerenciamentoPIM.Models
+{
+
+    public class Aluno
+    {
+        public int codigo { get; set; }
+        public string Nome { get; set; }
+        public string Disciplina { get; set; }
+
+
+
+
+        public bool Cadastrar(Aluno aluno)
+        {
+            try
+            {
+                string conexaoAccess = ConfigurationManager.ConnectionStrings["conexaoAccess"].ToString();
+
+                OleDbConnection conexaoDb = new OleDbConnection(conexaoAccess);
+
+                conexaoDb.Open();
+
+                string query = "INSERT INTO PIM_TABELA (aluno, disciplina) VALUES (@Nome, @Disciplina)";
+
+                OleDbCommand cmd = new OleDbCommand(query, conexaoDb);
+
+                var parametroDisciplina = cmd.CreateParameter();
+                parametroDisciplina.ParameterName = "@Disciplina";
+                parametroDisciplina.DbType = DbType.String;
+                parametroDisciplina.Value = aluno.Disciplina;
+                cmd.Parameters.Add(parametroDisciplina);
+
+                var parametroNome = cmd.CreateParameter();
+                parametroNome.ParameterName = "@Nome";
+                parametroNome.DbType = DbType.String;
+                parametroNome.Value = aluno.Nome;
+                cmd.Parameters.Add(parametroNome);
+
+
+
+
+                 if (cmd.ExecuteNonQuery() > 0)
+                {
+
+                    conexaoDb.Close();
+                    return true;
+                }
+
+                else
+                {
+                    conexaoDb.Close();
+                    return false;
+                }
+
+                 
+            }
+            catch (Exception ex)
+            {
+                return false;
+
+            }
+
+        }
+
+        public static void Consultar()
+        {
+            try
+            {
+                string conexaoAccess = ConfigurationManager.ConnectionStrings["conexaoAccess"].ToString();
+
+                OleDbConnection conexaoDb = new OleDbConnection(conexaoAccess);
+
+                conexaoDb.Open();
+
+                
+                string query = "SELECT aluno FROM PIM_TABELA";
+
+                OleDbCommand cmd = new OleDbCommand(query, conexaoDb);
+                OleDbDataReader getLista = cmd.ExecuteReader();
+
+
+                while (getLista.Read())
+                {
+                    var teste =getLista[0].ToString();
+                }
+                getLista.Close();
+
+
+            }
+            catch (Exception ex)
+            {
+               
+
+            }
+
+        }
+
+    }
+}
